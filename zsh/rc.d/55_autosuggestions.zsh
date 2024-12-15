614d2e40ef2c2fa5eb2428f048ad99f07b8e61b6
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-15 16:54:58 -0500

    testing zsh symlinks

--- zsh/rc.d/55_autosuggestions.zsh
+++ zsh/rc.d/55_autosuggestions.zsh
@@ -3,11 +3,11 @@ source "${ZDOTDIR}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # source "${ZDOTDIR}/plugins/autosuggestions-abbreviations-strategy/zsh-autosuggestions-abbreviations-strategy.zsh"
 
-# Don't rebind widgets by autosuggestion, it's already sourced pretty late
-ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+# # Don't rebind widgets by autosuggestion, it's already sourced pretty late
+# ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 
-# Clear suggestions after paste
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste)
+# # Clear suggestions after paste
+# ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste)
 
-# Enable additional suggestion strategies
-ZSH_AUTOSUGGEST_STRATEGY=(abbreviations history completion)
+# # Enable additional suggestion strategies
+# ZSH_AUTOSUGGEST_STRATEGY=(abbreviations history completion)

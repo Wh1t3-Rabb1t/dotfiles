6ec38fc6b23301743848780203293d112bcc2af2
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-18 20:53:12 -0500

    debugging fzf tab

--- zsh/rc.d/16_fzf_tab.zsh
+++ zsh/rc.d/16_fzf_tab.zsh
@@ -2,10 +2,10 @@
 source "${ZDOTDIR}/plugins/fzf-tab/fzf-tab.zsh"
 
 # Confirm selection and relaunch fzf-tab (space char)
-# zstyle ':fzf-tab:*' continuous-trigger ' '
+zstyle ':fzf-tab:*' continuous-trigger ' '
 
 # Set flags
-# zstyle ':fzf-tab:*' fzf-flags --no-preview --color=hl:reverse:#f38ba8
+zstyle ':fzf-tab:*' fzf-flags --no-preview --color=hl:reverse:#f38ba8
 
 
 

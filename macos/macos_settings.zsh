cf2c1e9dfb657ec18f6cb036ba95bfd91cf933a2
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-19 20:32:44 -0500

    testing macos default settings automation

--- macos/macos_settings.zsh
+++ macos/macos_settings.zsh
@@ -3,38 +3,41 @@
 # Quit System Preferences so it doesn't override settings
 osascript -e 'tell application "System Preferences" to quit'
 
-# Faster key repeat
-defaults write -g InitialKeyRepeat -int 12
-defaults write -g KeyRepeat -int 2
+# # Faster key repeat
+# defaults write -g InitialKeyRepeat -int 12
+# defaults write -g KeyRepeat -int 2
 
-# Display hidden files in Finder
-defaults write com.apple.finder AppleShowAllFiles True
+# # Display hidden files in Finder
+# defaults write com.apple.finder AppleShowAllFiles True
 
-# Default to the list view in Finder
-defaults write com.apple.finder FXPreferredViewStyle Nlsv
+# # Default to the list view in Finder
+# defaults write com.apple.finder FXPreferredViewStyle Nlsv
 
-# Show the full path at the bottom of Finder
-defaults write com.apple.finder ShowPathbar True
+# # Show the full path at the bottom of Finder
+# defaults write com.apple.finder ShowPathbar True
 
-# Show all file extensions
-# defaults write -g AppleShowAllExtensions -bool true
+# # Show all file extensions
+# # defaults write -g AppleShowAllExtensions -bool true
 
-# Unhide the ~/Library folder
-chflags nohidden ~/Library
+# # Unhide the ~/Library folder
+# chflags nohidden ~/Library
 
-# Hide the Dock
-defaults write com.apple.dock autohide True
+# # Hide the Dock
+# defaults write com.apple.dock autohide True
 
-# Don't show recent apps in the Dock
-# defaults write com.apple.dock show-recents -bool true
+# # Don't show recent apps in the Dock
+# # defaults write com.apple.dock show-recents -bool true
 
-# Hide the "Now Playing" menu bar item
-defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" False
+# # Hide the "Now Playing" menu bar item
+# defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" False
 
-# Save screenshots in the Screncaps folder
-defaults write com.apple.screencapture location ~/Documents/Screencaps
+# # Save screenshots in the Screncaps folder
+# defaults write com.apple.screencapture location ~/Documents/Screencaps
 
-# See the changes
-killall Dock
-killall Finder
-killall SystemUIServer
+# # See the changes
+# killall Dock
+# killall Finder
+# killall SystemUIServer
+
+
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/.local/dotfiles/macos/scha.jpg'" && killall Dock

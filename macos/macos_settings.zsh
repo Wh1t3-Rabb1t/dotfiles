76163d807e0b8fd3c4269181cb97d90dfbf4cac4
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-19 20:16:14 -0500

    testing macos default settings automation

--- macos/macos_settings.zsh
+++ macos/macos_settings.zsh
@@ -1,45 +1,40 @@
 #!/usr/bin/env zsh
 
 # Quit System Preferences so it doesn't override settings
-# osascript -e 'tell application "System Preferences" to quit'
+osascript -e 'tell application "System Preferences" to quit'
 
 # Faster key repeat
-# defaults write -g InitialKeyRepeat -int 12
-# defaults write -g KeyRepeat -int 2
-
-# # Disable press and hold for special characters
-# defaults write -g com.apple.finder AppleShowAllFiles -bool true
+defaults write -g InitialKeyRepeat -int 12
+defaults write -g KeyRepeat -int 2
 
 # Display hidden files in Finder
-# defaults write -g com.apple.finder AppleShowAllFiles -bool true
-
 defaults write com.apple.finder AppleShowAllFiles True
 
-# # Default to the list view in Finder
-# defaults write com.apple.finder FXPreferredViewStyle Nlsv
+# Default to the list view in Finder
+defaults write com.apple.finder FXPreferredViewStyle Nlsv
 
-# # Show the full path at the bottom of Finder
-# defaults write com.apple.finder ShowPathbar -bool true
+# Show the full path at the bottom of Finder
+defaults write com.apple.finder ShowPathbar True
 
-# # Show all file extensions
+# Show all file extensions
 # defaults write -g AppleShowAllExtensions -bool true
 
-# # Unhide the ~/Library folder
-# chflags nohidden ~/Library
+# Unhide the ~/Library folder
+chflags nohidden ~/Library
 
-# # Hide the Dock
-# defaults write com.apple.dock autohide -bool true
+# Hide the Dock
+defaults write com.apple.dock autohide True
 
-# # Don't show recent apps in the Dock
+# Don't show recent apps in the Dock
 # defaults write com.apple.dock show-recents -bool true
 
-# # Hide the "Now Playing" menu bar item
-# defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" -bool false
+# Hide the "Now Playing" menu bar item
+defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" False
 
 # Save screenshots in the Screncaps folder
-# defaults write com.apple.screencapture location ~/Documents/Screencaps
+defaults write com.apple.screencapture location ~/Documents/Screencaps
 
 # See the changes
-# killall Dock
+killall Dock
 killall Finder
-# killall SystemUIServer
+killall SystemUIServer

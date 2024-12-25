87fe94760c243a18cac47d3b1aaccaf03025fbdb
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-24 22:09:25 -0500

    testing macos default keybinding overrides

--- macos/macos_keybindings.zsh
+++ macos/macos_keybindings.zsh
@@ -6,4 +6,8 @@
 
 # sudo -v
 
-sudo defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "com.brave.Browser"; defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Tab" -string "^v"
+sudo defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "com.knollsoft.Rectangle"; \
+defaults com.knollsoft.Rectangle NSUserKeyEquivalents -dict-add "Left Half" -string "^v"
+
+# sudo defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "com.brave.Browser"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Tab" -string "^v"

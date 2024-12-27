c0e688b9f776c611cfd1f68590f64567f928b5d7
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-27 18:35:52 -0500

    testing macos default keybinding overrides

--- macos/macos_keybindings.zsh
+++ macos/macos_keybindings.zsh
@@ -9,31 +9,43 @@
 # ~ Option (⌥)
 # @ Command (⌘)
 
-# sudo -v
+sudo -v
 
-sudo defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "com.brave.Browser"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Tab" -string "~m"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Reopen Closed Tab" -string "$~m"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Window" -string "~n"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Private Window" -string "$~n"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Find..." -string "~f"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Search Tabs..." -string "~p"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Quick Commands" -string "$~p"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Close Tab" -string "~w"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Copy" -string "~c"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Cut" -string "~x"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Paste" -string "~v"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Undo" -string "~y"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Redo" -string "$~y"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Reload This Page" -string "~r"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Open Location..." -string "~'"; \
-defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Select All" -string "~a"
+defaults write com.apple.HIToolbox AppleSelectedInputSources -array-add '{ InputSourceKind = "Keyboard Layout"; "KeyboardLayout ID" = "-1"; "KeyboardLayout Name" = "Unicode Hex Input"; }'
+defaults write com.apple.HIToolbox AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.UnicodeHexInput"
+
+
+defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "org.mozilla.firefox"; \
+defaults write org.mozilla.firefox NSUserKeyEquivalents -dict-add "New Tab" -string "~m"; \
+defaults write org.mozilla.firefox NSUserKeyEquivalents -dict-add "New Window" -string "~n"; \
+defaults write org.mozilla.firefox NSUserKeyEquivalents -dict-add "New Private Window" -string "$~n"; \
+defaults write org.mozilla.firefox NSUserKeyEquivalents -dict-add "Find in Page..." -string "~f"; \
+defaults write org.mozilla.firefox NSUserKeyEquivalents -dict-add "Close Tab" -string "~w"
 
 killall SystemUIServer
 
 
+# sudo defaults write com.apple.universalaccess com.apple.custommenu.apps -array-add "com.brave.Browser"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Tab" -string "~m"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Reopen Closed Tab" -string "$~m"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Window" -string "~n"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "New Private Window" -string "$~n"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Find..." -string "~f"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Search Tabs..." -string "~p"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Quick Commands" -string "$~p"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Close Tab" -string "~w"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Copy" -string "~c"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Cut" -string "~x"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Paste" -string "~v"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Undo" -string "~y"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Redo" -string "$~y"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Reload This Page" -string "~r"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Open Location..." -string "~'"; \
+# defaults write com.brave.Browser NSUserKeyEquivalents -dict-add "Select All" -string "~a"
 
-# defaults write com.apple.HIToolbox AppleSelectedInputSources -array-add '{ InputSourceKind = "Keyboard Layout"; "KeyboardLayout ID" = "-1"; "KeyboardLayout Name" = "Unicode Hex Input"; }'
+
+
+
 
 # defaults write com.apple.HIToolbox AppleSelectedInputSources -array-add '{ \
 #     InputSourceKind = "Keyboard Layout"; \
@@ -41,7 +53,6 @@ killall SystemUIServer
 #     "KeyboardLayout Name" = "Unicode Hex Input"; \
 # }'
 
-# defaults write com.apple.HIToolbox AppleCurrentKeyboardLayoutInputSourceID -string "com.apple.keylayout.UnicodeHexInput"
 
 
 

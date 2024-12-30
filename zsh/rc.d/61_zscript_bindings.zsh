ffdecf8e263914bba5b3120128278b8a308cb168
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-30 03:32:15 -0500

    refactoring zsh config

--- zsh/rc.d/61_zscript_bindings.zsh
+++ zsh/rc.d/61_zscript_bindings.zsh
@@ -5,28 +5,34 @@
 #  /___|___/\___|_|  |_| .__/ \__| |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
 # =====================|_|======================================|___/========= #
 
+# CHEAT SHEAT
+# ---------------------------------------------------------------------------- #
+zle -N _cheat_sheet
+bindkey -M viins "^[c" _cheat_sheet         # Alt c = Cheat sheet
+
+
 # COMMAND HISTORY FZF
 # ---------------------------------------------------------------------------- #
-zle -N .cmd_history_fzf
-bindkey -M viins "^[p" .cmd_history_fzf      # Alt p = Command history fzf
+zle -N cmd_history_fzf
+bindkey -M viins "^[p" cmd_history_fzf      # Alt p = Command history fzf
 
 
 # TELEPORT
 # ---------------------------------------------------------------------------- #
-zle -N .teleport
-bindkey -M viins "^['" .teleport             # Alt ' = Z jump history fzf
+zle -N teleport
+bindkey -M viins "^['" teleport             # Alt ' = Z jump history fzf
 
 
 # RENAME FZF
 # ---------------------------------------------------------------------------- #
-zle -N .rename_fzf
-bindkey -M viins "^[r" .rename_fzf           # Alt r = Rename files / dirs in cwd
+zle -N rename_fzf
+bindkey -M viins "^[r" rename_fzf           # Alt r = Rename files / dirs in cwd
 
 
 # BROOT
 # ---------------------------------------------------------------------------- #
 if (( ${+commands[broot]} )); then
     source "${XDG_CONFIG_HOME}/broot/launcher/bash/br"
-    zle -N .broot_launcher
-    bindkey -M viins "^[f" .broot_launcher   # Alt f = Launch broot
+    zle -N broot_launcher
+    bindkey -M viins "^[f" broot_launcher   # Alt f = Launch broot
 fi

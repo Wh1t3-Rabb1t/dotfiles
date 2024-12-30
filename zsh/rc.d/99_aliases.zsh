e7fe106b9a4110b9f45684b0a950b70fc1bc2262
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2024-12-29 20:55:37 -0500

    major refactoring of zsh config underway

--- zsh/rc.d/99_aliases.zsh
+++ zsh/rc.d/99_aliases.zsh
@@ -8,26 +8,54 @@
 # Some handy suffix aliases
 # alias -s log=less
 
+
+# General
 alias h="cd ~"
 alias b="cd -"
+alias getip="ifconfig | grep inet"
 
+
+# Git
+# ---------------------------------------------------------------------------- #
+(( ${+commands[git]} )) && {
+    alias gg="add_commit_push"
+    alias gl="git log --all --graph --decorate --oneline"
+}
+
+
+# Lsd
+# ---------------------------------------------------------------------------- #
 (( ${+commands[lsd]} )) && {
     alias ls="echo ''; lsd -A -v"
     alias ll="echo ''; lsd -A --long"
     alias lt="echo ''; lsd -A --tree --depth"
 }
 
-alias gnug="grep -H -n -i --color=auto"
-alias dlv="yt-dlp -f mp4"
-alias dl720p="yt-dlp -S vcodec:h264,fps,res:720,acodec:m4a"
-alias dl1080p="yt-dlp -S vcodec:h264,fps,res:1080,acodec:m4a"
-alias getIP="ifconfig | grep inet"
-# alias syntaxTheme="fast-theme -l"
-# alias showAll="defaults write com.apple.finder AppleShowAllFiles True; killall Finder"
-# alias hideAll="defaults write com.apple.finder AppleShowAllFiles False; killall Finder"
+
+# Homebrew
+# ---------------------------------------------------------------------------- #
+(( ${+commands[brew]} )) && {
+    alias bo="brew outdated"
+    alias bu="brew upgrade"
+}
 
 
-# Functions
+# yt-dlp
+# ---------------------------------------------------------------------------- #
+(( ${+commands[yt-dlp]} )) && {
+    alias dlm="audio_in_opus"
+    alias dlpl="playlist_audio_and_concat"
+    alias dlfullpl="playlist_audio_individually"
+    alias dlv="yt-dlp -f mp4"
+    alias dl320p="yt-dlp -S vcodec:h264,fps,res:320,acodec:m4a"
+    alias dl480p="yt-dlp -S vcodec:h264,fps,res:480,acodec:m4a"
+    alias dl720p="yt-dlp -S vcodec:h264,fps,res:720,acodec:m4a"
+    alias dl1080p="yt-dlp -S vcodec:h264,fps,res:1080,acodec:m4a"
+}
+
+
+# zscripts
+# ---------------------------------------------------------------------------- #
 alias e="launch_nvim"
 alias f="find_and_open"
 alias d="find_and_goto"
@@ -39,35 +67,11 @@ alias nf="create_files"
 alias nd="create_dirs"
 alias rm="move_to_trash"
 
-# Git
-alias gg="add_commit_push"
-
-# yt-dlp
-alias dlm="audio_in_opus"
-alias dlpl="playlist_audio_and_concat"
-alias dlfullpl="playlist_audio_individually"
-
-
-# Plugin managers
-alias bo="brew outdated"
-alias bu="brew upgrade"
-# alias zpo="_zplugin_check_for_updates"
-# alias zpu="_zplugin_update"
-# alias zpa="_zplugin_add_new"
-# alias zpd="_zplugin_delete"
-
-
 
 
 # Override regular 'clear' with custom one, that puts prompt at bottom
 alias clear=clear-screen-soft-bottom
 
-# Prefer nvim when installed
-(( ${+commands[nvim]} )) && {
-    alias nv="nvim"
-    alias vi="nvim"
-    alias vim="nvim"
-}
 
 # Human file sizes
 (( ${+commands[df]} )) && alias df="df --human-readable --print-type"
@@ -84,17 +88,7 @@ alias clear=clear-screen-soft-bottom
 #     alias ls="ls --color=auto --hyperlink=auto --classify"
 #     alias ll="LC_COLLATE=C ls -l -v --almost-all --human-readable"
 # }
-
-# History suppression
-# (( ${+commands[clear]} )) && alias clear=" clear"
-# alias pwd=" pwd"
-# alias exit=" exit"
-
-# Safety
-# (( ${+commands[rm]} )) && alias rm="rm -I --preserve-root=all"
-
 # Suppress suggestions and globbing, enable wrappers
-# (( ${+commands[find]} )) && alias find="noglob find"
 (( ${+commands[touch]} )) && alias touch="nocorrect touch"
 (( ${+commands[mkdir]} )) && alias mkdir="nocorrect mkdir"
 (( ${+commands[cp]} )) && alias cp="nocorrect cp --verbose"

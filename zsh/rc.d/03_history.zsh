c2936bc74f7a1215a6bfde44de283cdbd2756fcc
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2025-01-01 02:57:58 -0500

    updated config

--- zsh/rc.d/03_history.zsh
+++ zsh/rc.d/03_history.zsh
@@ -7,6 +7,8 @@
 
 # !! History cannot be set in ~/.zshenv because it will
 # be overwritten by /etc/.zshrc in favour of macOS defaults
+
+# HISTFILE="${XDG_DATA_HOME}/zsh/.zsh_history"
 HISTFILE="${XDG_DATA_HOME}/zsh/history"
 HISTORY_IGNORE='(cd *|mv *|rm *|nf *|nd *|dl*)'
 HISTSIZE=10000

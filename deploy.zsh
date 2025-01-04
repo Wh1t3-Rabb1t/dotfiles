80afe946d88bc7d6098504bbea4e963fdb5e6650
Wh1t3-Rabb1t <88162211+Wh1t3-Rabb1t@users.noreply.github.com>
2025-01-03 20:06:54 -0500

    testing lsp tooling auto install

--- deploy.zsh
+++ deploy.zsh
@@ -27,7 +27,7 @@ XDG_STATE_HOME="${HOME}/.local/state"
 # Create required directories
 # ---------------------------------------------------------------------------- #
 print "\nCreating required directory tree...\n"
-# zf_mkdir -p "${XDG_CONFIG_HOME}"/nvim
+zf_mkdir -p "${XDG_CONFIG_HOME}"/nvim
 zf_mkdir -p "${XDG_CACHE_HOME}"/{vim/{backup,swap,undo},zsh}
 zf_mkdir -p "${XDG_DATA_HOME}"/{{goenv,jenv,luaenv,nodenv,phpenv,plenv,pyenv,rbenv}/plugins,zsh,man/man1,vim/spell,nvim/site/pack/plugins}
 zf_mkdir -p "${XDG_STATE_HOME}"
